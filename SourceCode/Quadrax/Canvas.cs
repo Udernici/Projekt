1f7676cbb5ad8b0e149a30118ba0eff022040a7a
larcele <janka.harvanova@gmail.com>
2016-03-20 21:29:57 +0100

    zaciatok navrhu tried

---
+++ SourceCode/Quadrax/Canvas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quadrax
+{
+    class MyCanvas : Panel
+    {
+
+        List<GameObject> _objects = new List<GameObject>();
+        Timer _gameTimer = new Timer();
+
+        public MyCanvas()
+        {
+
+        }
+
+        public void AddObject(GameObject o)
+        {
+            _objects.Add(o);
+        }
+
+        public void RemoveObject(GameObject o)
+        {
+            _objects.Remove(o);
+        }
+
+        public void Redraw()
+        {
+            //add graphic logic
+        }
+
+        public void Clear()
+        {
+            _objects.Clear();
+        }
+
+
+    }
+}
---
+++ SourceCode/Quadrax/GameObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrax
+{
+    abstract class GameObject : Object
+    {
+        int x;
+        int y;
+        bool solid;
+        int weight;
+        public GameObject(int x, int y, bool solid, int weight)
+        {
+            this.x = x;
+            this.y = y;
+            this.solid = solid;
+            this.weight = weight;
+        }
+
+        public int X { get { return this.x; } set { this.x = value; } }
+        public int Y { get { return this.y; } set { this.y = value; } }
+
+        //is collidable
+        public bool isSolid()
+        {
+            return solid;
+        }
+        
+        public bool canPush(int strength)
+        {
+            return strength >= weight;
+        }
+
+    }
+}
+
